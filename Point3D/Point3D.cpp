826450d2e33336212323a1538f228aaf8f5cc695
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-09-16 04:06:56 +0300

    Добавил 2 констуктора (без параметров/с параметрами)

--- Point3D/Point3D.cpp
+++ Point3D/Point3D.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 class Point3D {
 	int x, y, z;
+public:
+	Point3D()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	Point3D(int _x, int _y, int _z) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
 }
 ;
 int main(){
