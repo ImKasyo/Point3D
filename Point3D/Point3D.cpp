7e9888a7782494500cf2227f27db562fc8cbccc7
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-09-27 07:45:58 +0300

    В конструкторах добавил счетчик count при создании

--- Point3D/Point3D.cpp
+++ Point3D/Point3D.cpp
@@ -9,11 +9,13 @@ public:
 		x = 0;
 		y = 0;
 		z = 0;
+		count++;
 	}
 	Point3D(int _x, int _y, int _z) {
 		x = _x;
 		y = _y;
 		z = _z;
+		count++;
 	}
 	void Input(int x, int y, int z) {
 		this->x = x;
